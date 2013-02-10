479cd35458bb03088e309c5b39d56edfc6643011
soi33org <soi33.org@gmail.com>
2013-02-10 20:46:33 +0900

    サンプルプログラム更新
     - GPIOを使用するプログラムを更新

--- sample/sample.c
+++ sample/sample.c
@@ -6,17 +6,23 @@
  * Author: soi33org
  * Copyright (c) 2013, soi33org. All rights reserved.
  *****************************************************************/
+#include <unistd.h>
 #include "gpio_drv.h"
 
 int main(int argc, char *argv[])
 {
+    int i;
     gpio_init();
 
-    gpio_inout(7, E_GPIO_OUT);
-
-    gpio_write(7, 1);
-    sleep(5);
-    gpio_write(7, 0);
+    for(i = 7; i < 12; i++) {
+        gpio_inout(i, E_GPIO_OUT);
+    }
     
+    for(i = 7; i < 12; i++) {
+        gpio_write(i, 1);
+        sleep(1);
+        gpio_write(i, 0);
+    }
+
     return 0;
 }
